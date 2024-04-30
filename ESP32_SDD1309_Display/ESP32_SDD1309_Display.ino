35b4fdb02835226c9fe2d6bb5f641b9172a13b18
Simon <s224331@dtu.dk>
2024-04-30 14:35:48 +0200

    Changed the display code to work with the system

--- ESP32_SDD1309_Display/ESP32_SDD1309_Display.ino
+++ ESP32_SDD1309_Display/ESP32_SDD1309_Display.ino
@@ -28,74 +28,79 @@ void setup(void) {
 }
 
 void loop(void) {
-  static int selectedSpot = 0;
+  // Array representing the blinking state of parking spots (1 = no blink, 0 = blink)
+  int occSpots[4] = {0, 1, 0, 1};  // Only spot 3 will blink
+  unsigned long blinkStartTime = 0;  // Time variable to record the start time for blinking
+
   Welcome();
+  Serial.println("Hello idiot");
 
   if (Serial.available()) {
-    //Couldnt just parseInt since the new line fucked it uup so now I take in a string and convert it. 
-    String inputString = Serial.readStringUntil('\n'); 
-    inputString.trim(); 
-    int input = inputString.toInt(); 
+    // Reading a number from the serial monitor
+    String inputString = Serial.readStringUntil('\n');
+    
+    inputString.trim();
+    int input = inputString.toInt();
 
+    // Validate the input (input is ignored in your current request but might be used to trigger the action)
     if (input >= 1 && input <= 4) {
-      selectedSpot = input;
-      Serial.println(selectedSpot);
-      blinkStartTime = millis(); // Start the blink timer
-      drawParkingSpots(selectedSpot, blinkStartTime);
+      blinkStartTime = millis();  // Capture the current time
+      Serial.println("Triggering the display of parking spots.");
+      drawParkingSpots(occSpots, blinkStartTime);
+    } else {
+      noAccess();  // Handle invalid input
     }
-    else {
-      noAccess();
-    }
-    delay(2000);
+
+    //delay(2000); // Delay to avoid overwhelming the serial input
   }
 }
 
 
-void drawParkingSpots(int selectedSpot, unsigned long startTime) {
+
+void drawParkingSpots(int occSpots[], unsigned long startTime) {
   unsigned long endTime = startTime + 3000;  // Set end time for 3 seconds later
 
   while (millis() < endTime) {
-    u8g2.clearBuffer();  // Clear the buffer each cycle
+    u8g2.clearBuffer();
 
     int displayWidth = u8g2.getDisplayWidth();
     int topRoad = u8g2.getDisplayHeight() / 2;
-    int buttRoad = u8g2.getDisplayHeight() - 3; //Had to subtract 3 since the display was bugging and the line carried over. 
+    int buttRoad = u8g2.getDisplayHeight() - 3;
     int spotWidth = displayWidth / 4;
     int spotHeight = 25;
     int textOffsetX = (spotWidth / 4) - 3;
-    int textOffsetY = (spotHeight / 2) - 5; 
+    int textOffsetY = (spotHeight / 2) - 5;
 
-    // Draw the road lines
     u8g2.drawLine(0, topRoad, displayWidth - 3, topRoad);
     u8g2.drawLine(0, buttRoad, displayWidth - 3, buttRoad);
 
-    // Draw the dotted line in the middle of the road
     for (int x = 0; x < displayWidth; x += (3 + 5)) {
       u8g2.drawLine(x, topRoad + ((buttRoad - topRoad) / 2), x + 3, topRoad + ((buttRoad - topRoad) / 2));
     }
 
     u8g2.setFont(u8g2_font_ncenB08_tr);
 
-    // Draw the parking spots and handle the blinking
     for (int i = 0; i < 4; i++) {
       char spotLabel[3];
-      sprintf(spotLabel, "%d", i + 1);//plus 1 since the parking spots are from 1-4
+      sprintf(spotLabel, "%d", i + 1);
 
       int x = i * spotWidth + (spotWidth / 4);
       u8g2.drawFrame(x, topRoad - spotHeight + 1, spotWidth - (2 * (spotWidth / 4)), spotHeight);
-      u8g2.drawStr(x + textOffsetX, topRoad - textOffsetY, spotLabel);  //Puts in the parking spot numbers. 
+      u8g2.drawStr(x + textOffsetX, topRoad - textOffsetY, spotLabel);
 
-      if (i + 1 == selectedSpot) {
-        if ((millis() - startTime) / 100 % 2 == 0) { //Blink every 100 ms
-          u8g2.setDrawColor(0);//Blinking is done by turning it black
+      // Only blink the spot if the array element is 0
+      if (occSpots[i] == 0) {
+        if ((millis() - startTime) / 100 % 2 == 0) {  // Blink every 100 ms
+          u8g2.setDrawColor(0);
           u8g2.drawBox(x, topRoad - spotHeight + 1, spotWidth - (2 * (spotWidth / 4)), spotHeight);
           u8g2.setDrawColor(1);
         }
       }
     }
-    u8g2.sendBuffer();//send it     
+    u8g2.sendBuffer();
   }
 }
+
 
 
 void Welcome() {
